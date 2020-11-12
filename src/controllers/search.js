fc21adb034a5ff4f0b5382d025f8136296f8ca46
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-12 15:30:06 +0530

    Create search service

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,13 +1,17 @@
 // Load Express Router
 const router = require('express').Router();
 
+// Load Services
+const Search = require('../services/search');
+
 // Setup Routes
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 	let query = req.query.q || '';
 	query = query.trim().toLowerCase();
 
 	if (query) {
-		res.send(query);
+		const results = await Search.search(query);
+		res.send(results);
 	} else {
 		res.status(400).send();
 	}
---
+++ src/services/search.js
@@ -0,0 +1,9 @@
+// Create Search Class
+class Search {
+	static async search(query) {
+		return query;
+	}
+}
+
+// Export Search Class
+module.exports = Search;
