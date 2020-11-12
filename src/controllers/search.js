4bf76857630c0bdd2ac4edbfbe21f48d2f9c255b
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-12 15:06:56 +0530

    Validate query in search route

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -3,7 +3,14 @@ const router = require('express').Router();
 
 // Setup Routes
 router.get('/', (req, res) => {
-	res.send('hello');
+	let query = req.query.q || '';
+	query = query.trim().toLowerCase();
+
+	if (query) {
+		res.send(query);
+	} else {
+		res.status(400).send();
+	}
 });
 
 // Export Router
