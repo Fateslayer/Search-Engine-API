83980c2a021569be697109d8da6344e2e981b42d
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-12 14:04:29 +0530

    Setup dotenv

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+// Load Modules
 const express = require('express');
 
+// Load Environment Variables
+require('dotenv-flow').config();
+
+// Load Constants
+const { PORT } = process.env;
+
+// Setup Express
 const app = express();
 
+// Setup Routes
 app.get('/', (req, res) => {
 	res.send('hello');
 });
 
-app.listen(3000);
+// Start Server
+app.listen(PORT);
