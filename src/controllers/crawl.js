ec494fa43d97b0e1ef652cf4701918a9aebf451a
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-20 21:38:07 +0530

    Filter valid links before trying to add them

--- src/controllers/crawl.js
+++ src/controllers/crawl.js
@@ -5,9 +5,21 @@ const router = require('express').Router();
 const Crawl = require('../services/crawl');
 
 // Setup Routes
-router.post('/', async (req, res) => {
-	const result = await Crawl.addLinks();
-	res.send(result);
+router.post('/', async ({ body }, res) => {
+	let { links } = body;
+
+	if (Array.isArray(links)) {
+		links = Crawl.getValidLinks(links);
+
+		if (links.length > 0) {
+			const result = await Crawl.addLinks(links);
+			res.send(result);
+		} else {
+			res.status(400).send();
+		}
+	} else {
+		res.status(400).send();
+	}
 });
 
 // Export Router
--- src/services/crawl.js
+++ src/services/crawl.js
@@ -1,7 +1,16 @@
+// Load Modules
+const validator = require('validator');
+
 // Create Service
 class Crawl {
 	static async addLinks(links) {
 		return true;
+	}
+
+	static getValidLinks(links) {
+		return links
+			.map(link => link.toLowerCase().trim()) // Transform Links
+			.filter(link => validator.isURL(link)); // Filter Valid Links
 	}
 }
 
