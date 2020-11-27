a75e81e9a3a0292356377abeec9f99f68597dd87
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-27 20:09:43 +0530

    Implement page rank algorithm

--- src/controllers/rank.js
+++ src/controllers/rank.js
@@ -4,9 +4,9 @@ const { Rank: RankService } = require('../services');
 // Create Controller
 class Rank {
 	static async rankLinks(req, res) {
-		const count = await RankService.rankLinks();
-
-		res.send({ count });
+		const graph = await RankService.getGraph();
+		const ranks = await RankService.getRanks(graph);
+		res.send(ranks);
 	}
 }
 
--- src/services/rank.js
+++ src/services/rank.js
@@ -1,7 +1,56 @@
+// Load Models
+const { Link } = require('../models');
+
 // Create Service
 class Rank {
-	static async rankLinks() {
-		return 0;
+	static async getGraph() {
+		const graph = {};
+
+		const links = await Link.findAll({
+			attributes: ['id'],
+			include: {
+				model: Link,
+				as: 'child',
+				attributes: ['id'],
+			},
+		});
+
+		links.forEach(link => {
+			graph[link.id] = link.child.map(c => c.id);
+		});
+
+		return graph;
+	}
+
+	static async getRanks(graph, iterations = 5) {
+		let ranks = {};
+		const dF = 0.8;
+		const ids = Object.keys(graph);
+		const nLinks = ids.length;
+
+		ids.forEach(id => {
+			ranks[id] = 1 / nLinks;
+		});
+
+		for (let i = 0; i < iterations; ++i) {
+			const newRanks = {};
+
+			ids.forEach(id => {
+				let newRank = (1 - dF) / nLinks;
+
+				ids.forEach(node => {
+					if (graph[node].indexOf(+id) !== -1) {
+						newRank += dF * (ranks[node] / graph[node].length);
+					}
+				});
+
+				newRanks[id] = newRank;
+			});
+
+			ranks = newRanks;
+		}
+
+		return ranks;
 	}
 }
 
